66bd5defac70f27afa2e846050c497a8bc09ce62
Nicolas Rogeau <nicolas.rogeau@epfl.ch>
2021-03-10 14:44:45 +0100

    Upload assignment2 NRogeau

--- lecture_03/10_build_your_own_robot.py
+++ lecture_03/10_build_your_own_robot.py
@@ -1,7 +1,7 @@
 # Rhino
 from compas_fab.robots import Configuration
 from compas_rhino.artists import RobotModelArtist
-
+from compas_rhino.artists import CylinderArtist
 from compas.datastructures import Mesh
 from compas.geometry import Circle
 from compas.geometry import Cylinder
@@ -30,6 +30,7 @@ link1 = model.add_link("link1", visual_mesh=mesh, visual_color=(0.2, 0.5, 0.6))
 axis = (0, 0, 1)
 origin = Frame.worldXY()
 model.add_joint("joint1", Joint.CONTINUOUS, link0, link1, origin, axis)
+#continuous is a revolute joint without limits
 
 # add another link
 mesh = Mesh.from_shape(cylinder)  # create a copy!
@@ -39,13 +40,22 @@ link2 = model.add_link("link2", visual_mesh=mesh, visual_color=(0.5, 0.6, 0.2))
 origin = Frame((length, 0, 0), (1, 0, 0), (0, 1, 0))
 model.add_joint("joint2", Joint.CONTINUOUS, link1, link2, origin, axis)
 
+# add another link
+mesh = Mesh.from_shape(cylinder)  # create a copy!
+link3 = model.add_link("link3", visual_mesh=mesh, visual_color=(0.7, 0.5, 0.4))
+
+# add another joint to 'glue' the link to the chain
+origin = Frame((length, 0, 0), (1, 0, 0), (0, 1, 0))
+model.add_joint("joint3", Joint.CONTINUOUS, link2, link3, origin, axis)
+artist = RobotModelArtist(model)
 # Create a configuration object matching the number of joints in your model
-# configuration = ....
+configuration = Configuration.from_revolute_values([0,2,1], joint_names=['joint1','joint2','joint3'])
 
 # Update the model using the artist
-artist = RobotModelArtist(model)
-# artist.update ...
+artist = RobotModelArtist(model, layer='COMPAS:assignment2')
+artist.update(configuration.joint_dict)
 
 # Render everything
+artist.clear_layer()
 artist.draw_visual()
 artist.redraw()
