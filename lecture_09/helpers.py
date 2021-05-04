220bf448294080d29e4b561cada773e1c3734e45
Romana Rust <rust@arch.ethz.ch>
2021-05-04 20:27:54 +0200

    Update helpers.py

--- lecture_09/helpers.py
+++ lecture_09/helpers.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import time
 
+from compas.topology import breadth_first_ordering
+
 
 class Rate(object):
     def __init__(self, hz):
@@ -15,3 +17,30 @@ class Rate(object):
         if sleep_ns > 0:
             time.sleep(sleep_ns / 1e9)
         self.last_time = self.last_time + self.sleep_duration
+
+
+def traversal_linearly_ordered(assembly):
+    return sorted(assembly.nodes())
+
+
+def traversal_breadth_first_ordering(assembly):
+    root = assembly.leaves()[0]
+    return breadth_first_ordering(assembly.adjacency, root)
+
+
+def traversal_buildup_sequence(assembly):
+    visited = set()
+    elements = []
+    for key in assembly.nodes():
+        if assembly.degree_out(key) == 0:
+            get_dependencies(assembly, key, elements, visited)
+
+    return list(reversed(elements))
+
+
+def get_dependencies(assembly, key, elements, visited):
+    if key not in visited:
+        elements.append(key)
+        visited.add(key)
+    for n in assembly.neighbors_in(key):
+        get_dependencies(assembly, n, elements, visited)
